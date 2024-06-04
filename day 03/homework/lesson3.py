e4fc980b5ccc58c4b8173c843291597191ea189b
AvtandilJashi <j.avtandili1@gmail.com>
2024-06-04 18:05:16 +0400

    second commit

---
+++ day 03/homework/lesson3.py
@@ -0,0 +1,21 @@
+name = 'Avtandil'
+last_name = 'Jashi'
+age = 13
+hobby = 'programmer'
+country = 'Georgia'
+
+
+print(name, last_name +' '+  str(age)   +  hobby  +  country)
+
+
+
+print(5 * 5)
+print(7 * 7)
+print(50 // 4)
+print(120 * 9)
+print(9 ** 2)
+print(7 * 4)
+print(10 / 2)
+print(9 * 9)
+print(650 / 25)
+print(900 * 2)
