577c0e4c38ba1223b359bd5b618c9531d5c83f3c
AvtandilJashi <j.avtandili1@gmail.com>
2024-06-05 18:53:59 +0400

    commit

---
+++ day 05/homework/lesson5.py
@@ -0,0 +1,12 @@
+name = "my name Avtandil" #str
+
+surname = "i am Jashi" #str
+
+country = "i live in Georgia" #str
+
+hobby = "my hobby is playing basketball" #str
+
+age = 13 #int
+
+height = 170 #int
+print(name +" "+ surname +" "+ str(age) +" "+ str(height) +" "+ country +" "+ hobby) 
