03aa953780a7f7456f9f38440c741d577411e32c
AvtandilJashi <j.avtandili1@gmail.com>
2024-06-07 15:28:15 +0400

    commit

---
+++ day 07/homework/lesson7.py
@@ -0,0 +1,133 @@
+
+#თქვენს თავზე შემოიტანეთ ინფორმაცია: სახელი, გვარი, ასაკი, ქვეყანა, საყვარელი მანქანა, საყვარელი ფერი და ფავორიტი რიცხვი, შემოტანილი მნიშვნელობებით ააწყვეთ ერთი დიდი წინადადება
+
+
+name = "my name Avtandil" #str
+surname = "i am Jashi" #str
+
+country = "i live in Georgia" #str
+
+hobby = "my hobby is playing basketball" #str
+
+age = 13 #int
+
+car = "my favourite car is bentley"
+
+color = "my favourite color is black"
+
+number = "my favourite number is 9"
+
+height = 170 #int
+print(name +" "+ surname +" "+ str(age) +" "+ str(height) +" "+ country +" "+ hobby +" "+ car +" "+ color +" "+ str(number))
+  
+
+#მომხმარებელს შემოატანინეთ 3 რიცხვი და ამ 3 რიცხვზე გააკეთეთ 15 მაგალითი ყველა არითმეთიკურ ოპერატორზე.
+
+print(input("enter your number"))
+
+print(12 * 2)
+print(12 * 7)
+print(12 * 9)
+print(20 * 5)
+print(25 * 4)
+print(25 * 25)
+print(20 * 7)
+print(25 * 7)
+print(12 * 4)
+print(20 * 0)
+print(25 * 98)
+print(0 * 12)
+print(25 * 650)
+print(12 * 1)
+print(25 * 2)
+
+
+
+print(12 + 2)
+print(12 + 7)
+print(12 + 9)
+print(20 + 5)
+print(25 + 4)
+print(25 + 25)
+print(20 + 7)
+print(25 + 7)
+print(12 + 4)
+print(20 + 0)
+print(25 + 98)
+print(0 + 12)
+print(25 + 650)
+print(12 + 1)
+print(25 + 2)
+
+
+
+print(12 - 2)
+print(12 - 7)
+print(12 - 9)
+print(20 - 5)
+print(25 - 4)
+print(25 - 25)
+print(20 - 7)
+print(25 - 7)
+print(12 - 4)
+print(20 - 0)
+print(25 - 98)
+print(0 - 12)
+print(25 - 650)
+print(12 - 1)
+print(25 - 2)
+
+
+
+print(12 / 2)
+print(12 // 7)
+print(12 // 9)
+print(20 / 5)
+print(25 // 4)
+print(25 / 25)
+print(20 // 7)
+print(25 // 7)
+print(12 / 4)
+print(20 / 0)
+print(25 / 2)
+print(23 // 12)
+print(650 // 20)
+print(12 / 1)
+print(25 // 2)
+
+
+# ორ მომხმარებლის შორის ააგეთ დიალოგი ნებისმიერ თემატიკაზე
+
+name = 'levani'
+NAME = 'atuka'
+
+print(name,NAME)
+
+name - input('what is your name')
+surname - input('what is your surname')
+age = -input('what is your age')
+country = input('which country do you live')
+
+print('my name is', 'surname', 'age', 'country')
+
+name - int(input('enter x:'))
+surname - int(input('enter y:'))
+
+print(name + surname)
+
+
+str()
+int()
+
+
+
+x = '910'
+print(type(int(x)))
+
+name - 'gio'
+age = 16
+salary = 200
+
+print(type(name))
+print(type(age))
+print(type(salary))
