91f8aef0838886e42adc30f280226ac698ccc04f
AvtandilJashi <j.avtandili1@gmail.com>
2024-06-26 13:55:47 +0400

    commit

---
+++ day 13/homework/lesson13.py
@@ -0,0 +1,58 @@
+#გააკეთეთ 10 მაგალითი ლოგიკურ ოპერატორებზე(True and False  - ამ გზით არა)
+
+a = int(input('Enter number: '))
+b = int(input('Enter number: '))
+print(a < b)
+
+
+
+c = int(input('Enter number: '))
+d = int(input('Enter number: '))
+print(c > d)
+
+
+
+e = int(input('Enter number: '))
+f = int(input('Enter number: '))
+print(e < f)
+
+
+
+g = int(input('Enter number: '))
+h = int(input('Enter number: '))
+print(g > h)
+
+
+
+i = int(input('Enter number: '))
+j = int(input('Enter number: '))
+print(i < j)
+
+
+
+k = int(input('Enter number: '))
+l = int(input('Enter number: '))
+print(k > l)
+
+
+
+m = int(input('Enter number: '))
+n = int(input('Enter number: '))
+print(m < n)
+
+
+
+o = int(input('Enter number: '))
+p = int(input('Enter number: '))
+print(o > p)
+
+
+
+q = int(input('Enter number: '))
+r = int(input('Enter number: '))
+print(q < r)
+
+
+s = int(input('Enter number: '))
+t = int(input('Enter number: '))
+print(s > t)
