13c5df8f5054aaec1bcfab35fe399d93341d9cf0
AvtandilJashi <j.avtandili1@gmail.com>
2024-12-23 21:54:13 +0400

    commit

---
+++ day 80/classwork/script.js
@@ -0,0 +1,26 @@
+//შექმენით ცვლადები, სადაც შეიტანთ ინფორმაციას თქვენს თავზე, მაგ: სახელი, გვარი, ასაკი და ა.შშემდეგ ეს ცვლადები დაუმატეთ ერთმანეთს და გამოიტანეთ კონსოლში და ასევე alert - ში.
+
+let name = 'Avtandili';
+let surname = 'Jashi';
+let age = 14
+let fav_car = 'Bentley'
+let fav_color = 'Black'
+
+let all = name + surname + age + fav_car + fav_color;
+
+console.log(all);
+
+alert(all);
+
+
+
+//მარტივი კალკულატორი
+
+let x = 8; 
+let y = 9; 
+
+console.log("ჯამი:", x + y);
+console.log("სხვაობა:", x - y);
+console.log("ნამრავლი:", x * y);
+console.log("განაყოფი:", x / y);
+
